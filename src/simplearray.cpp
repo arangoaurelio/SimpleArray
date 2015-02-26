668fd26120a3f805f9becf840b0678a62949aa5b
arangoaurelio <arango.l.aurelio@gmail.com>
2015-02-26 15:51:31 -0800

    Simple array
    Simple Array initialization

---
+++ src/simplearray.cpp
@@ -0,0 +1,33 @@
+/*
+ * simplearray.cpp
+ *
+ *  Created on: Feb 26, 2015
+ *      Author: Aurelio Arango
+ *     Problem: Given an integer array remove any duplicates.
+ *
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int * remove_duplicates(int *array);
+
+int main()
+{
+
+	int numbers [] = {1,2,2,2,3,4,5};//setting up initial int array
+	int * array = numbers;
+	remove_duplicates(array);
+
+
+	return 0;
+}
+
+int * remove_duplicates(int * array)
+{
+
+
+	return array;
+}
